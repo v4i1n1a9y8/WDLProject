db4643e5c910bfa4a5fd65c69bbfd7e56938fc7b
vinay <vinaypatil41198@gmail.com>
2018-08-31 02:27:32 +0530

    tabs

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <?php 
 echo file_get_contents("modules/head1.html"); 
-echo "Home";
+?>
+HOME</title>
+<style>
+div.tab {
+    border: 2px solid lightblue;
+    border-radius: 20px;
+    margin:auto;
+
+    position: relative;
+    width: 300px;
+    height: 200px;
+    background: #aaa;
+    color: #000;
+    padding: 20px;  
+}
+a.tablink:link,a.tablink:visited,a.tablink:hover,a.tablink:active {
+    color: #ddd;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    font-size:40px;
+}
+div.tab:hover {
+    background-color: #ccc;
+}
+</style>
+<?php 
 echo file_get_contents("modules/head2.html");
 echo file_get_contents("modules/header.html");
 echo file_get_contents("modules/navigation.html");
@@ -8,6 +37,20 @@ echo '<div id="mainbody">';
 ?>
 
 
+<div class="tab">
+<p><a href="browse.php" class="tablink">
+    <div>
+    BROWSE
+    </div>
+</a></p>
+</div>
+<div class="tab">
+<p><a href="compare.php" class="tablink">
+    <div>
+    COMPARE
+    </div>
+</a></p>
+</div>
 
 
 <?php
