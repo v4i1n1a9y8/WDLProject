02acc0a5c5b2c8003bc6b39256d2d653931c1a77
= <=>
2018-09-17 01:49:59 +0530

    database done

--- browse.php
+++ browse.php
@@ -6,19 +6,43 @@ echo file_get_contents("modules/header.html");
 echo file_get_contents("modules/navigation.html");
 echo '<div id="mainbody">';
 ?>
+<?php    
+    try {
+        require "data/config.php";
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT name,company,processor,ram from mobiles";
+        $statement = $conn->query($sql);
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+
+?>
  <table class="blob">
-    <tr>
-        <th>Mobile Name</th>
-        <th>Processor</th>
-        <th>RAM</th>
-    </tr>
-    <?php for ($x = 0; $x <= 10; $x++) { ?>
+    <thead>
         <tr>
-            <td>Honor 7x</td>
-            <td>Kirin 659</td>
-            <td>4gb</td>
+            <th>Mobile Name</th>
+            <th>Company</th>
+            <th>Processor</th>
+            <th>RAM</th>
         </tr>
-    <?php }?> 
+    </thead>
+    <tbody>
+        <?php 
+        $rowCount = $statement->rowCount()?>
+        <?php for ($x = 0; $x < $rowCount; $x++) { 
+        $result =  $statement->fetchObject();
+        ?>
+            <tr>
+                <td><?php echo $result->name; ?></td>
+                <td><?php echo $result->company; ?></td>
+                <td><?php echo $result->processor; ?></td>
+                <td><?php echo $result->ram; ?></td>
+            </tr>
+        <?php } ?> 
+    <tbody>
 
 </table> 
 <?php
--- manage.php
+++ manage.php
@@ -7,24 +7,57 @@ echo file_get_contents("modules/navigation.html");
 echo '<div id="mainbody">';
 ?>
 
-<div id="blob" style="float: left">
- <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
+<div class="blob" style="float: left">
 
-try {
-    $conn = new PDO("mysql:host=$servername", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Database status: <strong style='color:green'>Connected</strong>" ;
+<?php
+    require "data/config.php";
+    try {
+        $conn = new PDO("mysql:host=$servername", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Database status: <strong style='color:green'>Connected</strong>" ;
+        }
+    catch(PDOException $e)
+        {
+            echo "Database status: <strong style='color:red'>Disconnected</strong>" ;
+        }
+?>
+
+<br><br><br>
+<form method="post">
+    <input type="submit" name="reset" value="Reset Database">
+</form>
+<?php 
+if(isset($_POST["reset"])){
+    try {
+        $sql = sprintf("DROP DATABASE %s",$dbname);
+        $conn->exec($sql);
+        $sql = sprintf("CREATE DATABASE %s",$dbname);
+        $conn->exec($sql);
+        $sql = "USE ".$dbname."; CREATE TABLE mobiles (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(30) NOT NULL,
+            company VARCHAR(30) NOT NULL,
+            processor VARCHAR(50),
+            ram VARCHAR(50),
+            date TIMESTAMP
+            )";
+        $conn->exec($sql);
+        $sql = "INSERT INTO mobiles (name, company,processor,ram,date)
+                VALUES ('Honor7X', 'Huawei', 'kirin 659','4gb',now())";
+        $conn->exec($sql);
+        $sql = "INSERT INTO mobiles (name, company,processor,ram,date)
+                VALUES ('Honor10', 'Apple', 'kirin infinite','1000gb',now())";
+        $conn->exec($sql);
     }
-catch(PDOException $e)
-    {
-        echo "Database status: <strong style='color:red'>Disconnected</strong>" ;
-    }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+}
 ?>
 </div>
+
+
 
 <?php
 echo '</div>';
