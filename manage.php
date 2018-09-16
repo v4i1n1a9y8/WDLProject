498a85481e2a0e3a7e69b6f27cadc3d6035b636b
= <=>
2018-09-16 23:04:55 +0530

    db connected

--- manage.php
+++ manage.php
@@ -7,7 +7,22 @@ echo file_get_contents("modules/navigation.html");
 echo '<div id="mainbody">';
 ?>
 
+ <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?> 
 
 <?php
 echo '</div>';
